c733107f648a698eb04780d6fad4ebfbe5c0eba1
urban-cougar <urbancougarltd@gmail.com>
2023-07-11 21:27:02 +0100

    Up all night furiously bashing out some stand up goodness
    * Our homepage is now a perfect meeting spot for all those
    that love tick tick ticking those boxes
    * The big finale, a beautiful winner gets squeezed out
    of the show page ;)

--- foo.py
+++ foo.py
@@ -1,10 +1,88 @@
 import sys
 import json
 import uuid
+import os
+import random
 from PySide2 import QtCore, QtWidgets, QtGui
-from PySide2.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView, QPushButton, QLabel, QStackedWidget, QFormLayout, QLineEdit
+from PySide2.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView, QPushButton, QLabel, QStackedWidget, QFormLayout, QLineEdit, QLineEdit
 from functools import partial
 from dataclasses import dataclass, asdict
+from typing import List
+
+os.environ["QT_IM_MODULE"] = "qtvirtualkeyboard"
+
+class StandupCreateView(QtWidgets.QWidget):
+    def __init__(self, peopleRepo):
+        super().__init__()
+        self.repo = peopleRepo
+        self.layout = QFormLayout()
+
+        self.setupTable()
+        self._renderTable()
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        
+        editPeopleBtn = QPushButton(text="Edit People", parent=self)
+        editPeopleBtn.clicked.connect(self.editPeople)
+        self.layout.addWidget(editPeopleBtn) 
+
+        startStandupBtn = QPushButton(text="Start Standup", parent=self)
+        startStandupBtn.clicked.connect(self.startStandup)
+        self.layout.addWidget(startStandupBtn)
+        
+        self.setLayout(self.layout)
+
+        self._chosenPeople = {}
+        for person in self.repo.getAll():
+            self._chosenPeople[person.id] = True
+
+    #Create table
+    def setupTable(self):
+        self.tableWidget = QTableWidget()
+
+        #Column count
+        self.tableWidget.setColumnCount(1)
+
+        #Table will fit the screen horizontally
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        self.tableWidget.itemClicked.connect(self.handlePersonClicked)
+
+    def _renderTable(self, people=None):
+        people = people or self.repo.getAll()
+        self.tableWidget.clear()
+
+        #Row count
+        self.tableWidget.setRowCount(len(people) + 1) 
+
+        # Set headings
+        self.tableWidget.setItem(0,0, QTableWidgetItem("Here?"))
+
+        for index, person in enumerate(people):
+            personHere = QTableWidgetItem(person.name)
+            personHere.personId = person.id
+            personHere.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled) 
+            personHere.setCheckState(QtCore.Qt.Checked)
+            self.tableWidget.setItem(index + 1, 0, personHere)
+
+    def handlePersonClicked(self, personRow):
+        self._chosenPeople[personRow.personId] = personRow.checkState() == QtCore.Qt.Checked
+
+    def editPeople(self):
+        masterLayout.showPeopleIndexView()
+
+    def startStandup(self):
+        standup = self.createStandup()
+        masterLayout.showStandupShowView(standup)
+
+    def createStandup(self):
+        people = []
+        for id in self._chosenPeople.keys():
+            if self._chosenPeople[id] == True:
+                people.append(self.repo.findById(id))
+
+        return Standup(people=people, winner=random.choice(people))
 
 class PeopleIndexView(QtWidgets.QWidget):
     def __init__(self, peopleRepo):
@@ -18,6 +96,10 @@ class PeopleIndexView(QtWidgets.QWidget):
         newPersonBtn = QPushButton(text="Add New Person", parent=self)
         newPersonBtn.clicked.connect(self.addNewPerson)
         self.layout.addWidget(newPersonBtn)
+
+        createStandupBtn = QPushButton(text="Back to Stand Up", parent=self)
+        createStandupBtn.clicked.connect(self.goBackToStandup)
+        self.layout.addWidget(createStandupBtn)
         
         self.setLayout(self.layout)
 
@@ -61,10 +143,13 @@ class PeopleIndexView(QtWidgets.QWidget):
 
 
     def editPerson(self, id):
-        masterLayout.showEditView(id)
+        masterLayout.showEditPersonView(id)
 
     def addNewPerson(self):
-        masterLayout.showNewView()
+        masterLayout.showNewPersonView()
+
+    def goBackToStandup(self):
+        masterLayout.showStandupCreateView()
 
     def reRender(self):
         self._renderTable()
@@ -86,7 +171,7 @@ class NewPersonView(QtWidgets.QWidget):
 
     def create(self):
         self.repo.create(Person(id = str(uuid.uuid4()), name = self.nameLineEdit.text()))
-        masterLayout.showIndexView()
+        masterLayout.showPeopleIndexView()
 
 
 class EditPersonView(QtWidgets.QWidget):
@@ -107,11 +192,29 @@ class EditPersonView(QtWidgets.QWidget):
     def update(self):
         self.person.name = self.nameLineEdit.text()
         self.repo.update(self.person)
-        masterLayout.showIndexView()
+        masterLayout.showPeopleIndexView()
 
     def loadPerson(self, id):
         self.person = self.repo.findById(id)
         self.nameLineEdit.setText(self.person.name)
+
+class StandupShowView(QtWidgets.QWidget):
+    def __init__(self, peopleRepo):
+        super().__init__()
+        self.repo = peopleRepo
+        self.layout = QFormLayout()
+        self.winnerLabel = QLabel()
+        self.layout.addRow(self.winnerLabel)
+        backBtn = QPushButton(text="Back", parent=self)
+        backBtn.clicked.connect(self.goBack)
+        self.layout.addRow(backBtn)
+        self.setLayout(self.layout)
+
+    def show(self, standup):
+        self.winnerLabel.setText(standup.winner.name)
+    
+    def goBack(self):
+        masterLayout.showStandupCreateView()
 
 
 
@@ -119,6 +222,11 @@ class EditPersonView(QtWidgets.QWidget):
 class Person:
     id: str
     name: str
+
+@dataclass
+class Standup:
+    people: List[Person]
+    winner: Person
 
 class PeopleRepository():
     def __init__(self, jsonFile):
@@ -171,27 +279,38 @@ class PeopleRepository():
             outfile.write(json.dumps(peopleDicts))
     
 class MasterLayout(QStackedWidget):
-    def __init__(self, peopleRepo, peopleIndexView, newPersonView, editPersonView):
+    def __init__(self, standupCreateView, peopleIndexView, newPersonView, editPersonView, standupShowView):
         super().__init__()
 
-        self.indexIndex = 0
-        self.newIndex = 1
-        self.editIndex = 2
+        self._standupCreateIndex = 0
+        self._peopleIndexIndex = 1
+        self._newPersonIndex = 2
+        self._editPersonIndex = 3
+        self._showStandupIndex = 4
+        self.addWidget(standupCreateView)
         self.addWidget(peopleIndexView)
         self.addWidget(newPersonView)
         self.addWidget(editPersonView)
+        self.addWidget(standupShowView)
         self.resize(800, 600)
+    
+    def showStandupCreateView(self):
+        self.setCurrentIndex(self._standupCreateIndex)
 
-    def showIndexView(self):
-        self.setCurrentIndex(self.indexIndex)
+    def showPeopleIndexView(self):
+        self.setCurrentIndex(self._peopleIndexIndex)
         self.currentWidget().reRender()
 
-    def showNewView(self):
-        self.setCurrentIndex(self.newIndex)
+    def showNewPersonView(self):
+        self.setCurrentIndex(self._newPersonIndex)
 
-    def showEditView(self, id):
-        self.setCurrentIndex(self.editIndex)
+    def showEditPersonView(self, id):
+        self.setCurrentIndex(self._editPersonIndex)
         self.currentWidget().loadPerson(id)
+
+    def showStandupShowView(self, standup):
+        self.setCurrentIndex(self._showStandupIndex)
+        self.currentWidget().show(standup)
         
 
 if __name__ == "__main__":
@@ -199,10 +318,11 @@ if __name__ == "__main__":
 
     peopleRepo = PeopleRepository("people.json")
     masterLayout = MasterLayout(
-        peopleRepo,
+        standupCreateView=StandupCreateView(peopleRepo),
         peopleIndexView=PeopleIndexView(peopleRepo),
         newPersonView=NewPersonView(peopleRepo),
-        editPersonView=EditPersonView(peopleRepo)
+        editPersonView=EditPersonView(peopleRepo),
+        standupShowView=StandupShowView(peopleRepo)
     )
     
     masterLayout.show()
@@ -226,10 +346,7 @@ if __name__ == "__main__":
 # make enter submit form
 
 ## NEXT ITEMS ##
-# "spin" and select a winner
-# store wins against a person
+# store standups
 # initiate a celebration upon win
-# virtual keyboard
 # Set viewport to correct size for pi
 # investigate how to style layouts
-# API calls for syncing people repo and standup repo
